df19fa59f66992bd71a007dceff0da6912582aef
lost22git <lost22git>
2024-02-13 18:34:17 +0800

    update

--- null.cr
+++ null.cr
@@ -1,5 +1,11 @@
 #!/usr/bin/env crystal
 
+#
+#  除非定义类型为 Nil 或者 联合类型 T | Nil (aka. T?)
+#
+#  否则值都不能为 nil
+#
+
 a : String? = "hello"
 p! a
 p! a.try &.upcase
@@ -7,3 +13,10 @@ p! a.try &.upcase
 a = nil
 p! a
 p! a.try &.upcase || "world"
+
+# if not-nil => if true
+if v = a
+  p "v is not nil"
+else
+  p "v is nil"
+end
